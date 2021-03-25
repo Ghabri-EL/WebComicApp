efe6afb3709af42b4a86c302d93ae20b60f27e04
Ghabri-EL <gherasimg15@gmail.com>
2021-03-25 02:33:56 +0000

    Added the method to flip the image horizontally

--- StayAtHomies/src/home.java
+++ StayAtHomies/src/home.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -7,22 +8,26 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
- 
+
+import java.awt.image.BufferedImage;
+
 public class home extends Application 
 {
 	private Stage stage;
 	private BorderPane layout;
 	private Scene scene;
+	private ImageView selectedCharacter = null;
 	ImageView leftChar = new ImageView();
 	ImageView rightChar = new ImageView();
+
     public static void main(String[] args) 
     {
         launch(args);
@@ -45,14 +50,15 @@ public class home extends Application
         scene = new Scene(layout);
         stage.setScene(scene);
         stage.setMinWidth(600);
-        stage.setMinHeight(400);
+        stage.setMinHeight(500);
         stage.setTitle("User Report 1");  
         stage.show(); 
     } 
     public void createMainPane()
     {
-    	Image image1 = new Image("images/neutral.png");
-    	Image image2 = new Image("images/joy.png");
+    	Image image1 = new Image("/images/neutral.png");
+    	Image image2 = new Image("/images/joy.png");
+    	System.out.println("WIDTH: " + image1.getWidth());
     	
     	leftChar.setFitHeight(300);
     	leftChar.setFitWidth(300);
@@ -62,13 +68,29 @@ public class home extends Application
     	
     	leftChar.setImage(image1);
     	rightChar.setImage(image2);
-    		
-    	
-    	
-    	
+
+    	leftChar.setStyle("-fx-border-color: black ;");
+    	rightChar.setStyle("-fx-border-color: black ;");
+        leftChar.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                selectedCharacter = leftChar;
+                System.out.println("LEFT SELECTED");
+                event.consume();
+            }
+        });
+
+        rightChar.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                selectedCharacter = rightChar;
+                System.out.println("RIGHT SELECTED");
+                event.consume();
+            }
+        });
+
     	HBox hbox = new HBox();
     	hbox.getChildren().addAll(leftChar,rightChar);
-    	
     	
     	hbox.setStyle("-fx-background-insets: 10 ;");
     	hbox.setAlignment(Pos.BOTTOM_CENTER);
@@ -131,23 +153,58 @@ public class home extends Application
     	scrollPane.setFitToHeight(true);
     	
     	Button btn1 = new Button();
-        btn1.setText("Rotate");
+        btn1.setText("RotateL");
         btn1.setPrefWidth(100);
         btn1.setPrefHeight(50);
         btn1.setOnAction(event -> 
         {
-        	rightChar.setRotate(90);
+            if(selectedCharacter != null){
+                double rotate = selectedCharacter.getRotate();
+                rotate -= 90;
+
+                if(rotate == 360.0 || rotate == -360.0){
+                    rotate = 0.0;
+                }
+                selectedCharacter.setRotate(rotate);
+            }
+            else{
+                System.out.println("Select one of the characters on which you want to perform the operation");
+            }
         });
         
         Button btn2 = new Button();
-        btn2.setText("BTN2");
+        btn2.setText("RotateR");
         btn2.setPrefWidth(100);
         btn2.setPrefHeight(50);
+        btn2.setOnAction(event ->
+        {
+            if(selectedCharacter != null){
+                double rotate = selectedCharacter.getRotate();
+                rotate += 90;
+
+                if(rotate == 360.0 || rotate == -360.0){
+                    rotate = 0.0;
+                }
+                selectedCharacter.setRotate(rotate);
+            }
+            else{
+                System.out.println("Select one of the characters on which you want to perform the operation");
+            }
+        });
         
         Button btn3 = new Button();
         btn3.setText("BTN3");
         btn3.setPrefWidth(100);
         btn3.setPrefHeight(50);
+        btn3.setOnAction(event ->{
+
+            if(selectedCharacter != null){
+                selectedCharacter.setImage(flipImage(selectedCharacter.getImage()));
+            }
+            else{
+                System.out.println("Select one of the characters on which you want to perform the operation");
+            }
+        });
         
         Button btn4 = new Button();
         btn4.setText("BTN4");
@@ -179,5 +236,21 @@ public class home extends Application
     	BorderPane buttons = new BorderPane(scrollPane);
     	
         layout.setLeft(buttons);
-    } 
+    }
+
+    private Image flipImage(Image image){
+        int width = (int) image.getWidth();
+        int height = (int)image.getHeight();
+
+        PixelReader pr = image.getPixelReader();
+        WritableImage img = new WritableImage(width,  height);
+        PixelWriter pw = img.getPixelWriter();
+
+        for(int i = 0; i < height; i++){
+            for(int j = 0; j < width; j++){
+                pw.setArgb(j, i, pr.getArgb((width - 1) - j, i));
+            }
+        }
+        return img;
+    }
 }
